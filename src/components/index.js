b8f4c385e787b70009582f9fad9fea6533c34b64
swazidr <wzd.rahman7@gmail.com>
2022-10-07 20:25:27 +0600

    for export all component from component with index.js

---
+++ src/components/index.js
@@ -0,0 +1,27 @@
+import Billing from './Billing';
+import Business from './Business';
+import Button from './CardDeal';
+import Client from './Clients';
+import CTA from './CTA';
+import FeedbackCard from './FeedbackCard';
+import Footer from './Footer';
+import GetStarted from './GetStarted';
+import Hero from './Hero';
+import Navbar from './Navbar';
+import Stats from './Stats';
+import Testimonial from './Testimonial';
+
+export {
+    Billing,
+    Business,
+    Button,
+    Client,
+    CTA,
+    FeedbackCard,
+    Footer,
+    GetStarted,
+    Hero,
+    Navbar,
+    Stats,
+    Testimonial
+}
